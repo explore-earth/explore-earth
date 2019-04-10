ef41bf3c5a357fedb104f09a01fe9a10bfb1e321
Mday1313 <melissaday076@gmail.com>
2019-04-10 19:21:09 -0400

    z-index corrected

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#modal").modal('show');
+});
+
+
+var config = {
+    apiKey: "AIzaSyBVduicjSagRcZbWga7LhS6zrCIq0OZYuw",
+    authDomain: "daredevils-project.firebaseapp.com",
+    databaseURL: "https://daredevils-project.firebaseio.com",
+    projectId: "daredevils-project",
+    storageBucket: "",
+    messagingSenderId: "746048417171"
+};
+firebase.initializeApp(config);
