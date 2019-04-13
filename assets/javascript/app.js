694da19ddecec28a22835c2b8153dd150d487641
Priscila Heller <priscila.x.p.r@gmail.com>
2019-04-13 17:33:23 -0400

    added authentication updated to the HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -74,34 +74,33 @@ $(function () {
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           // User is signed in.
-          var displayName = user.displayName;
-          var email = user.email;
-          var emailVerified = user.emailVerified;
-          var photoURL = user.photoURL;
-          var isAnonymous = user.isAnonymous;
-          var uid = user.uid;
-          var providerData = user.providerData;
+          var photoURL = $("<img>");
+            $("#user-avatar").attr("src","user.photoURL");
+            $("#user-avatar").append(photoURL);
+//           var email = user.email;
+//           var emailVerified = user.emailVerified;
+//           var photoURL = user.photoURL;
+//           var isAnonymous = user.isAnonymous;
+//           var uid = user.uid;
+//           var providerData = user.providerData;
           // [START_EXCLUDE]
-          document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-          document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-          document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+//           document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
+//           document.getElementById('quickstart-sign-in').textContent = 'Sign out';
+//           document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
           // [END_EXCLUDE]
         } else {
           // User is signed out.
           // [START_EXCLUDE]
-          document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-          document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-          document.getElementById('quickstart-account-details').textContent = 'null';
-          document.getElementById('quickstart-oauthtoken').textContent = 'null';
+          $("#user-avatar").empty;
           // [END_EXCLUDE]
         }
-        // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
-      });
-      // [END authstatelistener]
-      document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-    }
+//         // [START_EXCLUDE]
+//         document.getElementById('quickstart-sign-in').disabled = false;
+//         // [END_EXCLUDE]
+//       });
+//       // [END authstatelistener]
+//       document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
+//     }
     window.onload = function() {
       initApp();
     };
