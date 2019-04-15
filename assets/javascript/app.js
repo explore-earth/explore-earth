1896d9c2239e404c887a8eb0a29706a4b967bdc1
Greg Baker <semperfifcd@gmail.com>
2019-04-15 19:42:18 -0400

    Hard coded customs into index.html and added hiding and showing into the app.js.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,163 +23,199 @@ var config = {
   messagingSenderId: "746048417171"
 };
 firebase.initializeApp(config);
-firebase.auth().onAuthStateChanged(function(user){
-console.log(user);
-if(user){
-      console.log('We have a user');
-      var photoURL = $("<img>");
-      photoURL.attr("src", user.photoURL);
-      photoURL.attr("class","google-avatar");
-      photoURL.css({"width":"7%","height":"7%","border-radius":"50%"});
-      $("#user-avatar").append(photoURL);
-      // var displayName = user.displayName;
-      // $("#user-avatar").append(displayName); //this does not work yet
-      document.getElementById("customs").style.display = "block";
-      document.getElementById("journal").style.display = "block";
-      document.getElementById("menu-journal").style.display = "block";
-      document.getElementById("menu-login").style.display = "none";
-      document.getElementById("menu-logout").style.display = "block";
-
-      
-  
-}else{
-  console.log("We don't have a user");
-      $("#user-avatar").hide; ////this does not work yet
-      document.getElementById("customs").style.display = "none";
-      document.getElementById("journal").style.display = "none";
-      document.getElementById("menu-journal").style.display = "none";
-      document.getElementById("menu-logout").style.display = "none";
-      document.getElementById("menu-login").style.display = "block";
-      document.getElementById("user-avatar").style.display = "none";
-}
-});
-
-$("#menu-login").on('click',function(){
-console.log('this button works');
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-firebase.auth().signInWithPopup(provider)
-.then(function(user){
+firebase.auth().onAuthStateChanged(function (user) {
   console.log(user);
-}).catch(function(error){
-  console.log(error);
-})
-});
+  if (user) {
+    console.log('We have a user');
+    var photoURL = $("<img>");
+    photoURL.attr("src", user.photoURL);
+    photoURL.attr("class", "google-avatar");
+    photoURL.css({ "width": "7%", "height": "7%", "border-radius": "50%" });
+    $("#user-avatar").append(photoURL);
+    // var displayName = user.displayName;
+    // $("#user-avatar").append(displayName); //this does not work yet
+    document.getElementById("customs").style.display = "block";
+    document.getElementById("journal").style.display = "block";
+    document.getElementById("menu-journal").style.display = "block";
+    document.getElementById("menu-login").style.display = "none";
+    document.getElementById("menu-logout").style.display = "block";
 
-$("#menu-logout").on('click',function(){
-	var provider = new firebase.auth.GoogleAuthProvider();
-	console.log('logout works');
-	firebase.auth().signOut().then(function(){
-		document.getElementById("customs").style.display = "none";
-    document.getElementById("journal").style.display = "none";
-    document.getElementById("menu-journal").style.display = "none";
-    document.getElementById("menu-login").style.display = "block";
-	})
-});
 
 
-  window.onload = function () {
-    console.log("running");
+  } else {
+    console.log("We don't have a user");
+    $("#user-avatar").hide; ////this does not work yet
     document.getElementById("customs").style.display = "none";
     document.getElementById("journal").style.display = "none";
     document.getElementById("menu-journal").style.display = "none";
+    document.getElementById("menu-logout").style.display = "none";
+    document.getElementById("menu-login").style.display = "block";
+    document.getElementById("user-avatar").style.display = "none";
   }
+});
 
-  // ------------ AUTHENTICATION STUFF ENDS HERE -------- //
+$("#menu-login").on('click', function () {
+  console.log('this button works');
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+  firebase.auth().signInWithPopup(provider)
+    .then(function (user) {
+      console.log(user);
+    }).catch(function (error) {
+      console.log(error);
+    })
+});
 
-  var weatherURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=";
-  var weatherAPI = "Pw6sEtuGM1QQngSJFGOR9LFJLUtgnFhs";
-  var fixerURL;
-  var fixerAPI;
-  var unsplashURL;
-  var unsplashAPI;
-  var triposoURL;
-  var triposoAPI;
-  
-  var currentCity;
-  var country;
-  var locationKey;
-  
-  // =============================== Functions ===============================
-  // Get info for a city.
-  function getInfo(cityName) {
-      $.ajax({
-          url: weatherURL + weatherAPI + "&q=" + cityName,
-          method: "GET"
-      }).then(function (response) {
-          console.log(response);
-  
-          country = response[0].Country.EnglishName;
-          console.log("country is " + country);
-          // Check if country name has culture info.
-          switch (country) {
-              case "Brazil":
-                  console.log("it's in brazil");
-                  document.getElementById("customs").style.display = "block";
-                  $("#content").load("ajax/customs.html div#brazil");
-                  $('#curOne>option:eq(0)').prop('selected', true);
-                  $('#curOne>option:eq(1)').prop('selected', true);
-                  break;
-              case "Japan":
-                  console.log("it's in japan");
-                  document.getElementById("customs").style.display = "block";
-                  //$("#customs").load("ajax/customs.html div#japan");
-                  $('#curOne>option:eq(0)').prop('selected', true);
-                  $('#curOne>option:eq(3)').prop('selected', true);
-                  break;
-              case "United Arab Emirates":
-                  console.log("it's in uae");
-                  document.getElementById("customs").style.display = "block";
-                  $("#customs").load("ajax/customs.html div#uae");
-                  break;
-              case "Russia":
-                  console.log("it's in russia");
-                  document.getElementById("customs").style.display = "block";
-                  $("#customs").load("ajax/customs.html div#russia");
-                  $('#curOne>option:eq(0)').prop('selected', true);
-                  $('#curOne>option:eq(2)').prop('selected', true);
-                  break;
-              case "Spain":
-                  console.log("it's in spain");
-                  document.getElementById("customs").style.display = "block";
-                  $("#customs").load("ajax/customs.html div#spain");
-                  $('#curOne>option:eq(0)').prop('selected', true);
-                  $('#curOne>option:eq(2)').prop('selected', true);
-                  break;
-              default:
-                  console.log("default");
-                  break;
-  
-          }
-  
-          locationKey = response[0].Key;
-          console.log(locationKey);
-  
-          var latitude = response[0].GeoPosition.Latitude;
-          var longitude = response[0].GeoPosition.Longitude;
-  
-  
-          var mymap = L.map('mapid').setView([latitude, longitude], 13);
-          L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-              attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-              maxZoom: 18,
-              id: 'mapbox.streets',
-              accessToken: 'pk.eyJ1IjoiYXNoYW5ub24xOTkwIiwiYSI6ImNqdWg5d3E0bzB1bmE0NHMwMHB6eHphaXoifQ.gxpdB89rzE_fN7II-PhAiA'
-          }).addTo(mymap);
-  
-          $.ajax({
-              url: "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=Pw6sEtuGM1QQngSJFGOR9LFJLUtgnFhs",
-              method: "GET"
-          }).then(function (response) {
-              console.log(response);
-              $("#minMax").html(response.DailyForecasts[0].Temperature.Minimum.Value + " " + response.DailyForecasts[0].Temperature.Minimum.Unit + " / " + response.DailyForecasts[0].Temperature.Maximum.Value + " " + response.DailyForecasts[0].Temperature.Maximum.Unit);
-              $("#chanceRain").html(response.DailyForecasts[0].Day.IconPhrase);
-              console.log(response.DailyForecasts[0].Day.RainProbability);
-          });
-      });
-  
-  
-  }
+$("#menu-logout").on('click', function () {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  console.log('logout works');
+  firebase.auth().signOut().then(function () {
+    document.getElementById("customs").style.display = "none";
+    document.getElementById("journal").style.display = "none";
+    document.getElementById("menu-journal").style.display = "none";
+    document.getElementById("menu-login").style.display = "block";
+  })
+});
+
+
+window.onload = function () {
+  console.log("running");
+  document.getElementById("customs").style.display = "none";
+  document.getElementById("journal").style.display = "none";
+  document.getElementById("menu-journal").style.display = "none";
+}
+
+// ------------ AUTHENTICATION STUFF ENDS HERE -------- //
+
+var weatherURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=";
+var weatherAPI = "Pw6sEtuGM1QQngSJFGOR9LFJLUtgnFhs";
+var fixerURL;
+var fixerAPI;
+var unsplashURL;
+var unsplashAPI;
+var triposoURL;
+var triposoAPI;
+
+var currentCity;
+var country;
+var locationKey;
+
+// =============================== Functions ===============================
+// Get info for a city.
+function getInfo(cityName) {
+  $.ajax({
+    url: weatherURL + weatherAPI + "&q=" + cityName,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+
+    country = response[0].Country.EnglishName;
+    console.log("country is " + country);
+    // Check if country name has culture info.
+    switch (country) {
+      case "Brazil":
+        console.log("it's in brazil");
+        // Show customs
+        document.getElementById("customs").style.display = "block";
+
+        // Show customs for Brazil while hiding others
+        document.getElementById("brazil").style.display = "inline";
+        document.getElementById("japan").style.display = "none";
+        document.getElementById("uae").style.display = "none";
+        document.getElementById("russia").style.display = "none";
+        document.getElementById("spain").style.display = "none";
+
+
+        break;
+      case "Japan":
+        console.log("it's in japan");
+        // Show customs
+        document.getElementById("customs").style.display = "block";
+
+        // Show customs for Japan while hiding others
+        document.getElementById("brazil").style.display = "none";
+        document.getElementById("japan").style.display = "inline";
+        document.getElementById("uae").style.display = "none";
+        document.getElementById("russia").style.display = "none";
+        document.getElementById("spain").style.display = "none";
+
+        break;
+      case "United Arab Emirates":
+        console.log("it's in uae");
+        // Show customs
+        document.getElementById("customs").style.display = "block";
+
+        // Show customs for UAE while hiding others
+        document.getElementById("brazil").style.display = "none";
+        document.getElementById("japan").style.display = "none";
+        document.getElementById("uae").style.display = "block";
+        document.getElementById("russia").style.display = "none";
+        document.getElementById("spain").style.display = "none";
+
+        break;
+      case "Russia":
+        console.log("it's in russia");
+        // Show customs
+        document.getElementById("customs").style.display = "block";
+
+        // Show customs for Russia while hiding others
+        document.getElementById("brazil").style.display = "none";
+        document.getElementById("japan").style.display = "none";
+        document.getElementById("uae").style.display = "none";
+        document.getElementById("russia").style.display = "block";
+        document.getElementById("spain").style.display = "none";
+
+        break;
+      case "Spain":
+        console.log("it's in spain");
+        // Show customs
+        document.getElementById("customs").style.display = "block";
+
+        // Show customs for Spain while hiding others
+        document.getElementById("brazil").style.display = "none";
+        document.getElementById("japan").style.display = "none";
+        document.getElementById("uae").style.display = "none";
+        document.getElementById("russia").style.display = "none";
+        document.getElementById("spain").style.display = "block";
+
+        break;
+      default:
+        console.log("default");
+        // Hide customs
+        document.getElementById("customs").style.display = "none";
+
+        break;
+
+    }
+
+    locationKey = response[0].Key;
+    console.log(locationKey);
+
+    var latitude = response[0].GeoPosition.Latitude;
+    var longitude = response[0].GeoPosition.Longitude;
+
+
+    var mymap = L.map('mapid').setView([latitude, longitude], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiYXNoYW5ub24xOTkwIiwiYSI6ImNqdWg5d3E0bzB1bmE0NHMwMHB6eHphaXoifQ.gxpdB89rzE_fN7II-PhAiA'
+    }).addTo(mymap);
+
+    $.ajax({
+      url: "http://dataservice.accuweather.com/forecasts/v1/daily/1day/" + locationKey + "?apikey=Pw6sEtuGM1QQngSJFGOR9LFJLUtgnFhs",
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      $("#minMax").html(response.DailyForecasts[0].Temperature.Minimum.Value + " " + response.DailyForecasts[0].Temperature.Minimum.Unit + " / " + response.DailyForecasts[0].Temperature.Maximum.Value + " " + response.DailyForecasts[0].Temperature.Maximum.Unit);
+      $("#chanceRain").html(response.DailyForecasts[0].Day.IconPhrase);
+      console.log(response.DailyForecasts[0].Day.RainProbability);
+    });
+  });
+
+
+}
 
 
 $("#searchButton").on("click", function () {
